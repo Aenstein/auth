1701b08928da635da61aba07cfb5fd02cbd5faad
Aenstein <leonid.od@bk.ru>
2025-12-13 03:39:59 +0300

    proto + create handler

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,49 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net"
 
+	"github.com/brianvoe/gofakeit"
 	"github.com/fatih/color"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	desc "github.com/Aenstein/auth/pkg/note_v1"
 )
 
+const (
+	grpcPort = 50051
+)
+
+type server struct {
+	desc.UnimplementedNoteV1Server
+}
+
+func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	log.Printf("Role: %v, name: %v", req.Role, req.GetName())
+
+	return &desc.CreateResponse{
+		Id: gofakeit.Int64(),
+	}, nil
+}
+
 func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterNoteV1Server(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 	fmt.Println(color.GreenString("Hello, World!"))
 }

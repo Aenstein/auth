41e7d512963b62e480566b22cb960ce8ca47df53
Aenstein <leonid.od@bk.ru>
2025-12-15 02:49:53 +0300

    add Get, Update, Delete handlers

--- cmd/main.go
+++ cmd/main.go
@@ -10,6 +10,8 @@ import (
 	"github.com/fatih/color"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 
 	desc "github.com/Aenstein/auth/pkg/note_v1"
 )
@@ -28,6 +30,31 @@ func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.Cre
 	return &desc.CreateResponse{
 		Id: gofakeit.Int64(),
 	}, nil
+}
+
+func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+	log.Printf("Person id is: %v", req.GetId())
+
+	return &desc.GetResponse{
+		Id: req.GetId(),
+		Name: gofakeit.Name(),
+		Email: gofakeit.Email(),
+		Role: 0,
+		CreatedAt: timestamppb.New(gofakeit.Date()),
+		UpdatedAt: timestamppb.New(gofakeit.Date()),
+	}, nil
+}
+
+func (s *server) Update(ctx context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
+	log.Printf("Update person: %v", req.GetName())
+
+	return &emptypb.Empty{}, nil
+}
+
+func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("Id deleted user: %v", req.GetId())
+
+	return &emptypb.Empty{}, nil
 }
 
 func main() {
